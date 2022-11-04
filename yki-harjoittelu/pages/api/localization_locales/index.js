f50fe67fcd5c36240ac8d5723cc5a0a90988219c
Jannaten Nayem <jannaten@eduix.it>
2022-11-04 12:31:59 +0200

    redis server added

--- yki-harjoittelu/pages/api/localization_locales/index.js
+++ yki-harjoittelu/pages/api/localization_locales/index.js
@@ -1,3 +1,4 @@
+import { createClient } from 'redis';
 import { dbConnect } from '../../../utils';
 import { GenericService } from '../../../services';
 import { LocalizationLocale } from '../../../models';
@@ -5,14 +6,24 @@ import { localization_locale_schema as schema } from "../../../validation";
 
 export default async function handler(req, res) {
 	const { method } = req;
-	await dbConnect()
+	await dbConnect();
+	const DEFAULT_EXPIRATION = 360;
+	const client = createClient({ url: process.env.REDIS_ENDPOINT_URI, password: process.env.REDIS_AUTH });
+	client.on('error', (err) => console.log('Redis Client Error', err));
+	await client.connect();
 
 	const localizationLocaleService = new GenericService(LocalizationLocale);
 	switch (method) {
 		case 'GET':
 			try {
-				const locales = await localizationLocaleService.getAll();
-				res.status(200).send(locales);
+				const localeCache = await client.get('localization_locale');
+				if (localeCache != null) {
+					return res.send(JSON.parse(localeCache));
+				} else {
+					const locales = await localizationLocaleService.getAll();
+					client.setEx('localization_locale', DEFAULT_EXPIRATION, JSON.stringify(locales));
+					return res.status(200).send(locales);
+				}
 			} catch (error) {
 				res.status(500).send({ error });
 			}
@@ -22,7 +33,7 @@ export default async function handler(req, res) {
 				const { value, error } = schema.validate(req.body);
 				if (error) return res.status(400).send({ message: error.details[0].message });
 				const locale = await localizationLocaleService.add(value);
-				res.status(201).send(locale);
+				return res.status(201).send(locale);
 			} catch (error) {
 				res.status(500).send({ error });
 			}
