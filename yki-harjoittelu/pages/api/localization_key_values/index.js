8c33281ea33ed51173a655044932a98c21162545
Jannaten Nayem <jannaten@eduix.it>
2022-11-04 12:43:39 +0200

    redis is disabled for some moment now

--- yki-harjoittelu/pages/api/localization_key_values/index.js
+++ yki-harjoittelu/pages/api/localization_key_values/index.js
@@ -1,4 +1,4 @@
-import { createClient } from 'redis';
+// import { createClient } from 'redis';
 import { dbConnect } from '../../../utils';
 import { LocalizationKeyValues } from '../../../models';
 import { localization_key_values_schema as schema } from "../../../validation";
@@ -6,27 +6,27 @@ import { localization_key_values_schema as schema } from "../../../validation";
 export default async function handler(req, res) {
 	const { method } = req;
 	await dbConnect();
-	const DEFAULT_EXPIRATION = 360;
-	const client = createClient({ url: process.env.REDIS_ENDPOINT_URI, password: process.env.REDIS_AUTH });
-	client.on('error', (err) => console.log('Redis Client Error', err));
-	await client.connect();
+	// const DEFAULT_EXPIRATION = 360;
+	// const client = createClient({ url: process.env.REDIS_ENDPOINT_URI, password: process.env.REDIS_AUTH });
+	// client.on('error', (err) => console.log('Redis Client Error', err));
+	// await client.connect();
 
 	switch (method) {
 		case 'GET':
 			try {
-				const keyValuesCache = await client.get('localization_key_values');
-				if (keyValuesCache != null) {
-					return res.send(JSON.parse(keyValuesCache));
-				} else {
-					const keyValues = await LocalizationKeyValues.find().populate({
-						path: "locale_values",
-						populate: {
-							path: "language",
-						},
-					});
-					client.setEx('localization_key_values', DEFAULT_EXPIRATION, JSON.stringify(keyValues));
-					return res.send(keyValues);
-				}
+				// const keyValuesCache = await client.get('localization_key_values');
+				// if (keyValuesCache != null) {
+				// 	return res.send(JSON.parse(keyValuesCache));
+				// } else {
+				const keyValues = await LocalizationKeyValues.find().populate({
+					path: "locale_values",
+					populate: {
+						path: "language",
+					},
+				});
+				// client.setEx('localization_key_values', DEFAULT_EXPIRATION, JSON.stringify(keyValues));
+				return res.send(keyValues);
+				// }
 			} catch (error) {
 				res.status(500).send({ error });
 			}
